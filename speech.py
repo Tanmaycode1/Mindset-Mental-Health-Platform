b9b358ee50db6e4e9908b24b0252c8669f3b2b80
prtm1908 <prathambatra19@gmail.com>
2023-12-08 10:02:55 +0530

    llm

--- speech.py
+++ speech.py
@@ -1,37 +1,35 @@
 import speech_recognition as sr
-import openai
 import pyttsx3
 import pyaudio
 import time
 import os
 import sys
 
+from langchain import PromptTemplate, LLMChain
+from langchain import HuggingFaceHub
+from langchain.llms import GooglePalm
 
-openai.api_key = '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'
+
+llm = GooglePalm(google_api_key="AIzaSyARIhyYDaD_vneSbeYApUHQPFLApXMsIcY", temperature=0.1)
+
+template = """Task: Be a friend to a person. Talk about things that might calm down a person.
+Topic: friendship, yoga, meditation
+Style: Academic
+Tone: Calm
+Audience: 18-25 year olds
+Length: 3 lines
+Format: Text
+Here's the question. {question}
+"""
+
+prompt = PromptTemplate(template=template, input_variables=["question"])
+llm_chain = LLMChain(prompt=prompt, llm=llm, verbose=True)
+llm_chain = LLMChain(prompt=prompt, llm=llm, verbose=True)
 
 engine = pyttsx3.init()
 
-
-def one(filename):
-    r = sr.Recognizer()
-    with sr.AudioFile(filename) as source:
-        audio = r.record(source)
-    try:
-        return r.recognize_google(audio)
-    except:
-        print("skipping unknown error")
-
-
 def two(prompt):
-    response = openai.Completion.create(
-        engine="text-davinci-003",
-        prompt=prompt,
-        max_tokens=4000,
-        n=1,
-        stop=None,
-        temperature=0.5,
-    )
-    return response.choices[0].text
+    return (llm_chain(prompt)['text'])
 
 
 def three(text):
@@ -52,7 +50,7 @@ def four():
                         print(t)
                         print(f"you said {t}")
                         res = two(t)
-                        print(f"gpt says {res}")
+                        print(f"Virtual friend says {res}")
                         three(res)
                 else:
                     three("I didn't Get it")
